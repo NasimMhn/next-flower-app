0a308c3a4419672c651a0cf23b732ae10f6c44bf
nasimmhn <nasim.mahzoun@gmail.com>
2020-03-17 12:29:53 +0100

    debuging style component

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+
+// Styled components
+import styled from 'styled-components';
+
+
+const Header = () => {
+  return (
+    <div>
+      <Title>Flower</Title>
+    </div>
+  )
+}
+
+
+const Title = styled.h1`
+  
+`
+
+export default Header
